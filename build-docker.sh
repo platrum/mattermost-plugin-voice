4ac95926cf909b982d0406284bda0a2f4095bd8a
Lane Shukhov <rl.shukhov@icloud.com>
2024-10-10 21:11:46 +0300

    revert

--- build-docker.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-DOCKER_DEFAULT_PLATFORM=linux/amd64 docker build -t plugin-builder -f Dockerfile . && docker run --rm -v "$(pwd)":/app -v "/tmp/plugin-builder/go/path":/go -v "/tmp/plugin-builder/go/cache":/tmp/go/cache -w /app plugin-builder
